f7ce7ae0c79f5231420b60859d98e1fa307cee33
Christopher League <league@contrapunctus.net>
2013-06-23 11:28:11 -0700

    read environment from python

--- example/hello.py
+++ example/hello.py
@@ -1,6 +1,8 @@
 
 def hello_world(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    return ["Hello, world: ", str(environ)]
+    a = environ['SOME_BIT_OF_CONFIG']
+    b = environ['MY_SUPER_BIG_SECRET']
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return ["Hello, world.\n", a, "\n", b, "\n"]
 
 application = hello_world
