efd80b80600eb2ce4fb50f3ca42301acd8872215
Christopher League <league@contrapunctus.net>
2013-06-23 10:51:49 -0700

    fix python wsgi

--- example/hello.py
+++ example/hello.py
@@ -1,4 +1,6 @@
 
 def hello_world(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/html')])
-    return ["Hello, world."]
+    return ["Hello, world: ", str(environ)]
+
+application = hello_world
