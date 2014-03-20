316061e9c61766f09bf784293ecb0fe4720bcf8a
Christopher League <league@contrapunctus.net>
2014-03-20 03:39:46 +0000

    Revert "temporarily exclude django in example/"
    This reverts commit 37232b90107e55fba3d26c9506d41772c586e5ec.

--- example/hello.py
+++ example/hello.py
@@ -1,4 +1,5 @@
 import os
+from django import get_version
 
 def show_var(buf, v):
     buf.append(v)
@@ -9,6 +10,7 @@ def show_var(buf, v):
 def hello_world(environ, start_response):
     try:
         buf = ["Hello, world...!\n"]
+        buf.append('Django %s is available.\n' % get_version())
         show_var(buf, 'SOME_BIT_OF_CONFIG')
         show_var(buf, 'MY_SUPER_BIG_SECRET')
         start_response('200 OK', [('Content-Type', 'text/plain')])
